80945daf2443e620cebf152a12365a4e4ab3d1a0
Akanksha461 <ruhi461@GMAIL.COM>
2017-08-02 00:30:52 +0530

    lottery

--- headless_chrome.py
+++ headless_chrome.py
@@ -1,6 +1,7 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
@@ -9,6 +10,12 @@ chrome_options.add_argument("--headless")
 a= "/home/akanksha/Downloads/going_headless/chromedriver"
 driver = webdriver.Chrome(str(a),   chrome_options=chrome_options)
 #driver = webdriver.Chrome(str(a),   chrome_options=chrome_options)
-driver.get("https://www.facebook.com/")
+driver.get("https://dribbble.com/")
 title1=driver.title
 print(title1)
+
+driver.find_element_by_partial_link_text("Sign in").click()
+driver.find_element_by_id("login").send_keys("ruhi461")
+driver.find_element_by_id("password").send_keys("akankshaverma46")
+driver.find_element_by_css_selector('input[type="submit"]').click()
+driver.find_element_by_link_text("Shots").click()
