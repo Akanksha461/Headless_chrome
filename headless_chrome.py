12c6a954c78e36f96bd67abb85ee57ac1ef748f7
Akanksha verma <ruhi461@gmail.com>
2017-12-19 11:09:07 +0530

    Update headless_chrome.py

--- headless_chrome.py
+++ headless_chrome.py
@@ -1,21 +1,24 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+import time
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
-#chrome_options.binary_location = '/Applications/Google Chrome   Canary.app/Contents/MacOS/Google Chrome Canary'`
-a= "/home/akanksha/Downloads/going_headless/chromedriver"
+chrome_options.add_argument("headless")
+
+a = "/home/aj/going_headless/chromedriver"
+chrome_options.add_argument("--disable-notifications")
 driver = webdriver.Chrome(str(a),   chrome_options=chrome_options)
-#driver = webdriver.Chrome(str(a),   chrome_options=chrome_options)
-driver.get("https://dribbble.com/")
+driver.get("https://www.facebook.com/")
+driver.implicitly_wait (10)
+driver.find_element_by_id("email").send_keys("id")
+driver.find_element_by_id("pass").send_keys("password")
 title1=driver.title
 print(title1)
-
-driver.find_element_by_partial_link_text("Sign in").click()
-driver.find_element_by_id("login").send_keys("id")
-driver.find_element_by_id("password").send_keys("password")
-driver.find_element_by_css_selector('input[type="submit"]').click()
-driver.find_element_by_link_text("Shots").click()
+driver.find_element_by_id("loginbutton").click()
+time.sleep(4)
+driver.find_element_by_name("xhpc_message").send_keys("D")
+# driver.find_element_by_css_selector('[placeholder="Share an update, Akanksha."]').send_keys("hello")
+driver.find_element_by_xpath('(//*[@class="_2ph- _4-u3"])[2]').click()
+print('pagal')
