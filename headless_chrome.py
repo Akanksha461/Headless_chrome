84d3cc4f855c83d6a0420e176893c1ae6a3af3a9
Akanksha verma <ruhi461@gmail.com>
2018-01-25 16:50:00 +0530

    Update headless_chrome.py

--- headless_chrome.py
+++ headless_chrome.py
@@ -7,7 +7,7 @@ import time
 chrome_options = Options()
 chrome_options.add_argument("headless")
 
-a = "/home/aj/going_headless/chromedriver"
+a = "path/of/headless/chromedriver"
 chrome_options.add_argument("--disable-notifications")
 driver = webdriver.Chrome(str(a),   chrome_options=chrome_options)
 driver.get("https://www.facebook.com/")
@@ -21,4 +21,4 @@ time.sleep(4)
 driver.find_element_by_name("xhpc_message").send_keys("D")
 # driver.find_element_by_css_selector('[placeholder="Share an update, Akanksha."]').send_keys("hello")
 driver.find_element_by_xpath('(//*[@class="_2ph- _4-u3"])[2]').click()
-print('pagal')
+print('Sucess')
